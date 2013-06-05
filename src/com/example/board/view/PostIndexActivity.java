370d4831fa18fe6539bf14f6dfdf90a39a516884
everytime1004 <ha621209@naver.com>
2013-06-05 17:41:21 +0900

    asdf

--- src/com/example/board/view/PostIndexActivity.java
+++ src/com/example/board/view/PostIndexActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -23,6 +24,9 @@ import com.example.board.lib.UrlJsonAsyncTask;
 import com.example.board.model.NetworkInfo;
 import com.example.board.model.Post;
 import com.example.board.model.PostAdapter;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
 public class PostIndexActivity extends SherlockActivity {
 
@@ -30,13 +34,38 @@ public class PostIndexActivity extends SherlockActivity {
 
 	private String mCategory = null;
 
+	private PullToRefreshListView mPullRefreshListView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_post_index);
 
 		mCategory = getIntent().getStringExtra("category");
-		
+
+		mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
+
+		mPullRefreshListView
+				.setOnRefreshListener(new OnRefreshListener<ListView>() {
+					@Override
+					public void onRefresh(
+							PullToRefreshBase<ListView> refreshView) {
+						String label = DateUtils.formatDateTime(
+								getApplicationContext(),
+								System.currentTimeMillis(),
+								DateUtils.FORMAT_SHOW_TIME
+										| DateUtils.FORMAT_SHOW_DATE
+										| DateUtils.FORMAT_ABBREV_ALL);
+
+						// Update the LastUpdatedLabel
+						refreshView.getLoadingLayoutProxy()
+								.setLastUpdatedLabel(label);
+
+						// Do work to refresh the list here.
+						loadPostFromServer(NetworkInfo.TASKS_URL, mCategory);
+					}
+				});
+
 		setTitle(mCategory);
 
 		mPreferences = getSharedPreferences("AuthToken", MODE_PRIVATE);
@@ -47,7 +76,6 @@ public class PostIndexActivity extends SherlockActivity {
 			Toast.makeText(this, "로그인을 먼저 해주세요", Toast.LENGTH_LONG).show();
 			finish();
 		}
-
 	}
 
 	@Override
@@ -87,7 +115,7 @@ public class PostIndexActivity extends SherlockActivity {
 
 					if (category.equals(jsonTask.getString("category"))
 							|| jsonTask.getString("category").equals("공지사항")) {
-
+						
 						String updated_time = jsonTask.getString("updated_at");
 						// 2013-06-03T06:39:00Z
 
@@ -104,7 +132,7 @@ public class PostIndexActivity extends SherlockActivity {
 					}
 				}
 
-				ListView tasksListView = (ListView) findViewById(R.id.postLv);
+				PullToRefreshListView tasksListView = mPullRefreshListView;
 				if (tasksListView != null) {
 					tasksListView.setAdapter(new PostAdapter(
 							PostIndexActivity.this, tasksArray));
@@ -113,6 +141,8 @@ public class PostIndexActivity extends SherlockActivity {
 			} catch (Exception e) {
 				Toast.makeText(context, "게시물이 없습니다.", Toast.LENGTH_LONG).show();
 			} finally {
+				mPullRefreshListView.onRefreshComplete();
+
 				super.onPostExecute(json);
 			}
 		}
